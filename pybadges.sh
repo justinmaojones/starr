12703500901712df98aebc9ce1f6981136ae5e3e
Justin Mao-Jones <justinmaojones@gmail.com>
2020-11-04 23:10:00 -0800

    embed logo

--- pybadges.sh
+++ pybadges.sh
@@ -3,4 +3,5 @@ python -m pybadges \
     --right-text="2.7, 3.6, 3.7, 3.8, 3.9" \
     --whole-link="https://www.python.org/" \
     --logo='https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/python.svg' \
-    > docs/badges/pybadges.svg
+    --embed-logo \
+    > docs/badges/python.svg
