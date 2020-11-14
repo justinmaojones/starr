d56f09e97d5459b96d0112883a02a52a9c222f2d
Justin Mao-Jones <justinmaojones@gmail.com>
2020-11-14 12:27:30 -0800

    cleanup

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,6 +2,7 @@
 set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
+# run from root
 coverage run -m pytest
 coverage report -m
 coverage-badge -f -o ./docs/badges/coverage.svg
