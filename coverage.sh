1460b492085ad7ba264f12f3a6f509227ab26fda
Justin Mao-Jones <justinmaojones@gmail.com>
2020-11-14 11:03:11 -0800

    exit on error for coverage

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
 coverage run -m pytest
 coverage report -m
 coverage-badge -f -o ./docs/badges/coverage.svg
