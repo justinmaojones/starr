af55d5dd27259f188b7de069cbc7f97688980c8e
Justin Mao-Jones <justinmaojones@gmail.com>
2021-07-15 23:57:44 -0700

    add readme documentation

--- setup.py
+++ setup.py
@@ -10,12 +10,28 @@ try:
 except IOError:
     extras_require = None
 
+README = open("README.md").read() 
+
 setup(
     name="starr",
     version="0.1",
     description="Sumtrees for NumPy arrays",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justinmaojones/starr",
     author="Justin Mao-Jones",
     author_email="justinmaojones@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     install_requires=requirements,
     extras_require=extras_require,
     packages=find_packages(include=["starr", "starr.*"]),
