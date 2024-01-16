f5b864cdb31bb056ee8dcecf360607e584cfc677
Justin Mao-Jones <justinmaojones@gmail.com>
2024-01-16 20:59:09 +0000

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def get_long_description():
 
 setup(
     name="starr",
-    version="0.2.1",
+    version="0.3.1",
     description="Sumtrees for NumPy arrays",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
